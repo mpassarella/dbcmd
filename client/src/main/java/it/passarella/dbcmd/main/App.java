9bb69cab49e585c2daf2c2b92bef000c1afe7e1d
Manuel Passarella <manuel.passarella@primeur.com>
2019-09-24 15:50:50 +0200

    Some changes

--- client/src/main/java/it/passarella/dbcmd/main/App.java
+++ client/src/main/java/it/passarella/dbcmd/main/App.java
@@ -8,43 +8,41 @@ public class App {
 
         StringBuilder command = new StringBuilder("");
         Scanner input = null;
-
         CommandDispatcher cd = null;
-        
         boolean finished = false;
 
-
-        if(args.length > 0) {
-
-
-            if(args.length == 3) {
-                
-                cd = CommandDispatcher.getInstance(args[0], args[1], args[2], System.out); 
-                input = new Scanner(System.in);
-                
-            } else if(args.length == 1) {
-                
+        switch(args.length)
+        {
+            case 1:
                 cd = CommandDispatcher.getInstance(args[0], System.out);            
                 input = new Scanner(System.in);
-            } 
+            break;
 
-            while(!finished) {
+            case 3:
+                cd = CommandDispatcher.getInstance(args[0], args[1], args[2], System.out); 
+                input = new Scanner(System.in);
+            break;
 
-                System.out.print("SQL>");
-                command.append(input.nextLine());
-                
-                finished = cd.manage(command.toString());
-                
-                command = new StringBuilder("");
-            }
-        } else {
-
-            usage();
+            default:
+                usage();
+                System.exit(0);
         }
+
+        while(!finished) 
+        {
+            System.out.print("SQL>");
+            command.append(input.nextLine());
+            
+            finished = cd.manage(command.toString());
+            
+            command = new StringBuilder("");
+        }
+
+        System.exit(0);
     }
 
-    public static void usage() {
-        
+    public static void usage() 
+    {
         System.out.println("DBCMD USAGE SHEET");
         System.out.println("java -jar dbcmd.jar connectionString username password");
         System.out.println("java -jar dbcmd.jar propertyFile");
